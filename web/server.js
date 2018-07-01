0822855b6d2ff353140ba4addeb84fb1b195edc6
Zal Machado <zalmachado@gmail.com>
2018-06-30 23:28:07 -0400

    Add worker container

--- web/server.js
+++ web/server.js
@@ -1,15 +1,11 @@
 const path = require('path');
 const express = require('express');
-var Twitter = require('twitter')
-const Sentiment = require('sentiment')
-const _ = require('lodash')
-const teams = require('testsocket-teams')
 
 const app = express();
 const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-const sentiment = new Sentiment();
-const port = process.env.PORT || 8000;
+const io = require('socket.io-client');
+const emitter = require('socket.io')(server);
+const port = 8000;
 
 let dailyData = {
     all: []
@@ -30,79 +26,16 @@ app.get('/*', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.ht
 
 server.listen(port);
 
-var client = new Twitter({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+const socket = io('http://worker:8200');
+socket.on('tweet', (tweet) => {
+    const { team, player, score } = tweet
+
+    dailyData['all'].unshift({ player, score })
+    if (dailyData['all'].length > 200) dailyData['all'].pop()
+
+    const teamRecent = dailyData[team] || []
+    teamRecent.unshift({ player, score })
+    if (teamRecent.length > 200) teamRecent.pop()
+    dailyData[team] = teamRecent
+    emitter.emit('tweet', tweet);
 })
-
-const sentimentOverrides = {
-    beast: 3,
-    mvp: 3,
-    clutch: 2,
-    trash: -3,
-    superstar: 1,
-    crazy: 0,
-    cut: 0,
-    deficit: 0,
-    fire: 3,
-    '🔥': 1,
-    cooking: 1
-}
-
-const tagsToTeam = teams.tagsToTeam;
-const rosters = teams.rosters;
-
-const tags = Object.keys(tagsToTeam)
-
-client.stream('statuses/filter', { track: tags.join(), stall_warnings: true }, function(stream) {
-    stream.on('data', function (data) {
-        if (data.text && !data.retweeted_status) {
-            tweet = data.text.toLowerCase()
-            tags.forEach(tag => {
-                if (tweet.includes(tag)) {
-                    let mentionedTeam = tagsToTeam[tag];
-                    let mentionedPlayer = null;
-                    const roster = rosters[mentionedTeam] || []
-                    roster.some(name => {
-                        if (tweet.includes(name)) mentionedPlayer = name;
-                        if (mentionedPlayer) {
-                            let score = sentiment.analyze(tweet, { extras: sentimentOverrides }).score
-                            if (mentionedPlayer === 'kevin love') score -= 3
-                            console.log(tweet + ' ' + score + ' ' + mentionedPlayer)
-                            io.emit('tweet', {
-                                team: mentionedTeam,
-                                player: mentionedPlayer,
-                                tweetInfo: {
-                                    tweetId: data.id,
-                                    username: data.user.name,
-                                    screenName: data.user.screen_name,
-                                    profilePic: data.user.profile_image_url_https,
-                                    text: data.text
-                                },
-                                score: score
-                            });
-
-                            dailyData['all'].unshift({ player: mentionedPlayer, score })
-                            if (dailyData['all'].length > 200) dailyData['all'].pop()
-                            const teamRecent = dailyData[mentionedTeam] || []
-                            teamRecent.unshift({ player: mentionedPlayer, score })
-                            if (teamRecent.length > 200) teamRecent.pop()
-                            dailyData[mentionedTeam] = teamRecent
-                        }
-                        return !!mentionedPlayer
-                    })
-                }
-            })
-        }
-    });
-
-    stream.on('warning', function(warning) {
-        console.log("WARNING: " + warning)
-    })
-
-    stream.on('error', function(error) {
-        console.log(error)
-    })
-});
