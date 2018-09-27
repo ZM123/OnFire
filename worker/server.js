68ce705a68ef1ff27712a57b0aba5378a9cb9704
Zal Machado <zalmachado@gmail.com>
2018-09-27 16:16:24 -0400

    use node-tweet-stream

--- worker/server.js
+++ worker/server.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const express = require('express');
-var Twitter = require('twitter')
+var Twitter = require('node-tweet-stream')
 const Sentiment = require('sentiment')
 const _ = require('lodash')
 const teams = require('testsocket-teams')
@@ -16,8 +16,8 @@ server.listen(port);
 var client = new Twitter({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+    token: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 })
 
 const sentimentOverrides = {
@@ -40,60 +40,60 @@ const rosters = teams.rosters;
 
 const tags = Object.keys(tagsToTeam)
 
-client.stream('statuses/filter', { track: tags.join(), stall_warnings: true }, function(stream) {
-    stream.on('data', function (data) {
-        const isTweet = _.conformsTo(data, {
-          user: _.isObject,
-          id_str: _.isString,
-          text: _.isString,
-        })
+client.track(tags)
 
-        if (isTweet) {
-            if (data.text && !data.retweeted_status) {
-                tweet = data.text.toLowerCase()
-                tags.forEach(tag => {
-                    if (tweet.includes(tag)) {
-                        let mentionedTeam = tagsToTeam[tag];
-                        let mentionedPlayer = null;
-                        const roster = rosters[mentionedTeam] || []
-                        roster.some(name => {
-                            if (tweet.includes(name)) mentionedPlayer = name;
-                            if (mentionedPlayer) {
-                                let score = sentiment.analyze(tweet, { extras: sentimentOverrides }).score
-                                if (mentionedPlayer === 'kevin love' || mentionedPlayer === 'kyle love') score -= 3
-                                // console.log(tweet + ' ' + score + ' ' + mentionedPlayer)
-                                io.emit('tweet', {
-                                    team: mentionedTeam,
-                                    player: mentionedPlayer,
-                                    tweetInfo: {
-                                        tweetId: data.id,
-                                        username: data.user.name,
-                                        screenName: data.user.screen_name,
-                                        profilePic: data.user.profile_image_url_https,
-                                        text: data.text
-                                    },
-                                    score: score
-                                });
-                            }
-                            return !!mentionedPlayer
-                        })
-                    }
-                })
-            }
-        } else {
-            if (!data.limit) console.log("OTHER: " + JSON.stringify(data))
+client.on('tweet', function (data) {
+    const isTweet = _.conformsTo(data, {
+      user: _.isObject,
+      id_str: _.isString,
+      text: _.isString,
+    })
+
+    if (isTweet) {
+        if (data.text && !data.retweeted_status) {
+            tweet = data.text.toLowerCase()
+            tags.forEach(tag => {
+                if (tweet.includes(tag)) {
+                    let mentionedTeam = tagsToTeam[tag];
+                    let mentionedPlayer = null;
+                    const roster = rosters[mentionedTeam] || []
+                    roster.some(name => {
+                        if (tweet.includes(name)) mentionedPlayer = name;
+                        if (mentionedPlayer) {
+                            let score = sentiment.analyze(tweet, { extras: sentimentOverrides }).score
+                            if (mentionedPlayer === 'kevin love' || mentionedPlayer === 'kyle love') score -= 3
+                            // console.log(tweet + ' ' + score + ' ' + mentionedPlayer)
+                            io.emit('tweet', {
+                                team: mentionedTeam,
+                                player: mentionedPlayer,
+                                tweetInfo: {
+                                    tweetId: data.id,
+                                    username: data.user.name,
+                                    screenName: data.user.screen_name,
+                                    profilePic: data.user.profile_image_url_https,
+                                    text: data.text
+                                },
+                                score: score
+                            });
+                        }
+                        return !!mentionedPlayer
+                    })
+                }
+            })
         }
-    });
-
-    stream.on('warning', function(warning) {
-        console.log("WARNING: " + JSON.stringify(warning))
-    })
-
-    stream.on('error', function(error) {
-        console.log(JSON.stringify(error))
-    })
-
-    stream.on('end', function(end) {
-        console.log("END: " + JSON.stringify(end))
-    })
+    } else {
+        if (!data.limit) console.log("OTHER: " + JSON.stringify(data))
+    }
 });
+
+client.on('warning', function(warning) {
+    console.log("WARNING: " + JSON.stringify(warning))
+})
+
+client.on('error', function(error) {
+    console.log(JSON.stringify(error))
+})
+
+client.on('end', function(end) {
+    console.log("END: " + JSON.stringify(end))
+})
